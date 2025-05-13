7766bf01c374bddd566da812962fd2757dd801c1
zakzaks <ariefn82@gmail.com>
2025-05-13 08:27:58 +0700

    Revert "feat: added React + Inertia"

--- resources/js/Pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold text-green-500">Welcome to My App</h1>
-            <p className="mt-4 text-lg">This is a simple React app with Tailwind CSS.</p>
-        </div>
-    );
-}
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/js/app.jsx"],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
-        react(),
     ],
 });
